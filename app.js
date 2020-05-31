f25d88c9e60b5d8fcaf8f5cbbcd1255c29f7b831
Pritam Das <pd7662@gmail.com>
2020-05-31 18:37:25 +0530

    added ocr.js

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const prompt = require('prompt-sync');
 const scrap = require('./lib/scrap')
+const Ocr = require('./lib/ocr')
 let comicLink = 'https://rawdevart.com/comic/against-gods/chapter-185/';
 let apiKey = 'ca9d212ae488957';
 let comicLanguage = 'chs';
 (async () => {
   try {
-  let scrapData = scrap(comicLink);
+  let scrapData = await scrap(comicLink);
+  let ocrData = await Ocr(apiKey,comicLanguage,scrapData.chapterLink);
+  console.log(ocrData[0]);
+
   } catch (error) {
-    console.log(error);
+    console.log('app' + error);
   }
 })();
