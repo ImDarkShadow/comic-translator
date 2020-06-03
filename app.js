469c2fbfa1a0890936e80503d85ed24f3aa52ae0
Pritam Das <pd7662@gmail.com>
2020-06-03 12:37:27 +0530

    added print.js --- beta stage

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const prompt = require('prompt-sync');
 const scrap = require('./lib/scrap');
 const Ocr = require('./lib/ocr');
 const Chinese = require('./language/chinese');
-const  translate = require('./lib/translate')
+const translate = require('./lib/translate');
+const print = require('./lib/print');
 let comicLink = 'https://rawdevart.com/comic/against-gods/chapter-185/';
 let apiKey = 'ca9d212ae488957';
 let comicLanguage = 'chs';
@@ -22,8 +23,17 @@ let comicLanguage = 'chs';
 
     }
     const translatedText = await translate(cluster[0]);
-    console.log(translatedText);
-
+    //console.log(translatedText);
+    for(let imageNumber = 1,translatedIndex = 0; imageNumber < cluster.length ; imageNumber++) {
+      for(clusterNumber = 0; clusterNumber < cluster[imageNumber].length -1; clusterNumber++){
+        cluster[imageNumber][clusterNumber].clusterText = translatedText[translatedIndex];
+        translatedIndex++;
+      }
+    }
+    //console.log(cluster[1][0])
+    //cluster.splice(0,1);
+    //console.log(cluster[1]);
+    const printedText = print(cluster,scrapData.chapterLink);
   } catch (error) {
     console.log('app' + error);
   }
